ea310af12202019834ead5ba4f03ad0a903d5dbc
Vyacheslav Likashkov <xb0ksx@gmail.com>
2022-03-04 00:19:36 +0300

    feat: add Preview view

--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -1,3 +1,48 @@
 <template>
-  <div></div>
+  <div class="preview">
+    <h2 class="preview__title">Персональные данные</h2>
+    <p class="preview__desc">Василий, 30 лет</p>
+    <h2 class="preview__title preview__title--bottom">Дети</h2>
+    <div class="preview__children">
+      <div class="preview__child preview__desc">Петр, 14 лет</div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.preview {
+  max-width: 616px;
+  margin: 0 auto;
+  padding-top: 30px;
+
+  &__title {
+    margin-bottom: 20px;
+    font-size: 16px;
+    font-weight: 500;
+
+    &--bottom {
+      margin-top: 60px;
+      margin-bottom: 0;
+    }
+  }
+
+  &__desc {
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  &__children {
+    display: inline-flex;
+    flex-direction: column;
+  }
+
+  &__child {
+    height: 44px;
+    padding: 10px 20px;
+    margin-top: 20px;
+    display: inline-block;
+    background: #f1f1f1;
+    border-radius: 5px;
+  }
+}
+</style>
