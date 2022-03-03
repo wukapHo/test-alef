2dc56f7d7fa311f5b7e1d319443d10c262242eec
Vyacheslav Likashkov <xb0ksx@gmail.com>
2022-03-03 20:47:10 +0300

    feat: add header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,55 @@
 <template>
-  <div></div>
+  <div class="line"></div>
+  <div class="container">
+    <header-app />
+    <router-view />
+  </div>
+  <footer-app />
 </template>
 
-<style lang="scss"></style>
+<script>
+import HeaderApp from "./components/HeaderApp.vue";
+import FooterApp from "./components/FooterApp.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    HeaderApp,
+    FooterApp,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  list-style: none;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  color: #111111;
+  text-decoration: none;
+}
+
+.container {
+  max-width: 1366px;
+  padding: 0 20px;
+  margin: 0 auto;
+}
+
+.line {
+  width: 100%;
+  height: 76px;
+  position: absolute;
+  box-shadow: 0px 1px 0px rgba(17, 17, 17, 0.1);
+}
+</style>
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">all rights reserved</footer>
+</template>
+
+<style lang="scss">
+.footer {
+  width: 100%;
+  height: 76px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fafafa;
+  box-shadow: 0px 1px 0px rgba(17, 17, 17, 0.1);
+}
+</style>
+
+<script>
+export default {
+  name: "FooterApp",
+};
+</script>
